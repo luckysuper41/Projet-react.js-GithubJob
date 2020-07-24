04864bed5632dabcb07830b5629282736781f323
NGUYEN Huynh Dung <57275058+luckysuper41@users.noreply.github.com>
2020-07-25 01:20:22 +0200

    Updates

---
+++ precache-manifest.93e1bc4d40d749a3e2e24714e9f131b3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "44db57eafe516a178f8d5e8069851b39",
+    "url": "/Projet-react.js-GithubJob/index.html"
+  },
+  {
+    "revision": "7d7683398cd72b6767ef",
+    "url": "/Projet-react.js-GithubJob/static/css/main.21e45724.chunk.css"
+  },
+  {
+    "revision": "55a1ff654485bf637159",
+    "url": "/Projet-react.js-GithubJob/static/js/2.c839f931.chunk.js"
+  },
+  {
+    "revision": "8ebb70227fec3b61ad3af3d99ebe8940",
+    "url": "/Projet-react.js-GithubJob/static/js/2.c839f931.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7d7683398cd72b6767ef",
+    "url": "/Projet-react.js-GithubJob/static/js/main.bde743c8.chunk.js"
+  },
+  {
+    "revision": "9d979b958fb39b9c3ae0",
+    "url": "/Projet-react.js-GithubJob/static/js/runtime-main.5ed7a10d.js"
+  }
+]);
